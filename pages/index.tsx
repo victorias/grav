b300dee8c4d420898ace10fbd27eec4470eb24ac
victorias <victoria.s.sun@gmail.com>
2020-10-15 11:24:57 -0700

    Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import styled from "@emotion/styled";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+const Stack = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`;
 type Props = {};
 
 const Index = ({}: Props) => {
-  return <div>hi</div>;
+  return (
+    <Stack>
+      <Hero />
+    </Stack>
+  );
 };
 
 export default Index;
